c953e7709af26b7245c9ca7e8da7620924b0df5b
Mushroom King <xiayingp@gmail.com>
2020-05-18 12:21:52 -0700

    Parser is done

--- parser/README.md
+++ parser/README.md
@@ -1,12 +1,32 @@
-# Study Section 1
+# Pattern Matching Engine Overview
+Define context free grammar.
+Implement a LR parser
+Implement a pattern matching function, with a few lambda helpers.
+
+## How the search flow works
+Given pattern in string: `a*`, and a text to search.
+
+Parse the pattern to API format (string).
+Example: `star(lit(‘a’))`
+
+Call pattern matching:
+`eval` converts the string API format to actual functions.
+
+Use the provided compiled lambdas, like `alt`, `star`, `lit`, etc.
+
+Calculate results, find the smallest reminder, return the longest match words.
+
+
+## Study Section 1
+### Choose start symbol and patterns
 ```
 RE => seq RE | seq
 ```
 
-## What if I only do `RE => seq RE`
+### What if I only do `RE => seq RE`
 Result:
 ```
-['seq', 'RE'] a*b*c*
+['seq', 'RE’] a*b*c*
 debug: ['\\w+'] lit ['a']
 debug: ['lit'] unit [['lit', 'a']]
 debug: ['unit', '[*]'] star [['unit', ['lit', 'a']], '*']
@@ -27,13 +47,13 @@ debug: ['star'] seq [['star', ['unit', ['lit', 'c']], '*']]
 Reminder text is '', which is empty string. But we still want to search for `seq`,
 so we got None as result.
 
-### The fix
+### The solution
 At the last step, we want to switch to another alternative.
-That is, at reminder text is `c*`, we do not want to search in `['seq', 'RE']`.
+That is, at reminder text is `c*`, we do not want to search in `[‘seq’, ‘RE’]`.
 Instead, we want to search in `[seq]` only.
 Result is good then:
 ```
-['seq'] c*
+[‘seq’] c*
 debug: ['\\w+'] lit ['c']
 debug: ['lit'] unit [['lit', 'c']]
 debug: ['unit', '[*]'] star [['unit', ['lit', 'c']], '*']
@@ -43,5 +63,3 @@ debug: ['seq', 'RE'] RE [['seq', ['star', ['unit', ['lit', 'b']], '*']], ['RE', 
 debug: ['seq', 'RE'] RE [['seq', ['star', ['unit', ['lit', 'a']], '*']], ['RE', ['seq', ['star', ['unit', ['lit', 'b']], '*']], ['RE', ['seq', ['star', ['unit', ['lit', 'c']], '*']]]]]
 ```
 Thus, we want to have pattern: `RE => seq RE | seq`.
-
-
--- parser/parser.py
+++ parser/parser.py
@@ -1,47 +1,13 @@
 # ---------------
-# User Instructions
-#
-# In this problem, you will be using many of the tools and techniques
-# that you developed in unit 3 to write a grammar that will allow
-# us to write a parser for the JSON language.
-#
-# You will have to visit json.org to see the JSON grammar. It is not
-# presented in the correct format for our grammar function, so you
-# will need to translate it.
-
+# A Regex Engine
 # ---------------
-# Provided functions
-#
-# These are all functions that were built in unit 3. They will help
-# you as you write the grammar.  Add your code at line 102.
 
 from functools import update_wrapper
 from string import split
 import re
 import pprint as pp
+import sys
 
-# def convert(tree):
-# 	"Convert the tree to the regex API format used in class."
-# 	if isinstance(tree, tuple): tree = tree[0] # Remove () and ending ''.
-# 	seq = tree[1:] # Remove opening 'RE'.
-# 	if len(seq) == 1: return parse_unit(seq[0])	# Single-unit sequences
-# 	return "seq(%s, %s)" % (parse_unit(seq[0]), parse_unit(seq[1]))
-#
-# def parse_unit(unit):
-# 	"Parse an individual unit of a regex pattern."
-# 	type = unit[0]
-# 	if type == 'RE': return convert(unit)
-# 	if type == 'expr': return parse_unit(unit[1])
-# 	if type == 'group': return convert(unit[2])
-# 	if type == 'star': return "star(%s)" % parse_unit(unit[1][1])
-# 	if type == 'plus': return "plus(%s)" % parse_unit(unit[1][1])
-# 	if type == 'opt': return "opt(%s)" % parse_unit(unit[1][1])
-# 	if type == 'alt': return "alt(%s, %s)" % (parse_unit(unit[2][1]), parse_unit(unit[4][1]))
-# 	if type == 'oneof': return "oneof('%s')" % unit[2][1]
-# 	if type == 'lits' or type == 'lit': return "lit('%s')" % unit[1]
-# 	if type == 'dot': return "oneof('?')"
-# 	if type == 'eol': return "eol('$')"
-# 	return "Unable to parse unit."
 
 def grammar(description, whitespace=r'\s*'):
   """Convert a description to a grammar.  Each line is a rule for a
@@ -64,7 +30,7 @@ def grammar(description, whitespace=r'\s*'):
     lhs, rhs = split(line, ' => ', 1)
     alternatives = split(rhs, ' | ')
     G[lhs] = tuple(map(split, alternatives))
-  #pp.pprint(G)
+  # pp.pprint(G)
   return G
 
 
@@ -119,11 +85,11 @@ def parse(start_symbol, text, grammar):
   def parse_atom(atom, text):
     if atom in grammar:  # Non-Terminal: tuple of alternatives
       for alternative in grammar[atom]:
-        if atom == 'RE':
-          print alternative, text
+        # if atom == 'RE':
+        #  print alternative, text
         tree, rem = parse_sequence(alternative, text)
         if rem is not None:
-          print 'debug:', alternative, atom, tree
+          # print 'debug:', alternative, atom, tree
           return [atom] + tree, rem
 
       return Fail
@@ -163,56 +129,178 @@ def parse_re(pattern):
 def convert(tree):
   # Convert the tree to the regex API format used in class."
   pp.pprint(tree)
-  top = tree[1] if tree[0] == 'RE' else None
+  # Preprocessing only once to remove () and ending ''.
+  if isinstance(tree, tuple):
+    tree = tree[0]
+
   results = []
-  post_order_convert(top, results)
+  process_expr(tree, results)
+  if not results: return None
+  if len(results) == 1:
+    return results[0]
+  elif len(results) == 2:
+    return "seq(%s, %s)" % (results[0], results[1])
 
-  print 'Result set:', results
-  print 'APIs', ''.join([e for e in results])
+  return None
 
 
+def process_expr(tree, results):
+  if not tree: return
+  root = tree[0]
+  if root == 'expr':
+    results.append(convert(tree[1]))
+  elif root == 'unit':
+    results.append(convert(tree[1]))
+  elif root == 'oneof':
+    results.append("oneof('%s')" % tree[2][1])
+  elif root == 'star':
+    results.append("star(%s)" % convert(tree[1]))
+  elif root == 'opt':
+    results.append("opt(%s)" % convert(tree[1]))
+  elif root == 'alt':
+    results.append("alt(%s, %s)" % (convert(tree[2][1]), convert(tree[4][1])))
+  elif root == 'plus':
+    results.append("plus(%s)" % convert(tree[1]))
+  elif root == 'eol':
+    results.append("eol('$')")
+  elif root == 'dot':
+    results.append("oneof('?')")
+  elif root == 'group':
+    results.append(convert(tree[2]))
+  elif root == 'RE':
+    results.append(convert(tree[1]))
+    if len(tree) > 2:
+      results.append(convert(tree[2]))
+  elif root == 'lit':
+    results.append("lit('%s')" % tree[1])
+  else:
+    print "un-handled key:", root
 
-def post_order_convert(tree, results):
-  if tree and isinstance(tree, list):
-    results.append('(')
-    post_order_convert(tree[0], results)
-    results.append(')')
-    results.append('(')
-    post_order_convert(tree[1], results)
-    results.append(')')
-  elif isinstance(tree, str):
-    results.append(tree)
-    print tree
+  return None
 
 
 def test_convert():
   tree1 = ['RE', ['expr', ['lit', 'hello']]]
   tree2 = ['RE',
-            ['expr', ['star', ['unit', ['lit', 'a']], '*']],
-            ['RE',
-             ['expr', ['star', ['unit', ['lit', 'b']], '*']],
-             ['RE', ['expr', ['star', ['unit', ['lit', 'c']], '*']]]
+           ['expr', ['star', ['unit', ['lit', 'a']], '*']],
+           ['RE',
+            ['expr', ['star', ['unit', ['lit', 'b']], '*']],
+            ['RE', ['expr', ['star', ['unit', ['lit', 'c']], '*']]]
             ]
-          ]
-  print convert(tree2)
+           ]
+  tree3 = ['RE', ['expr', ['star', ['unit', ['oneof', '[', ['lit', 'ab'], ']']], '*']]]
+
+  tree4 = ['RE',
+           ['expr', ['oneof', '[', ['lit', 'bcfhrsm'], ']']],
+           ['RE', ['expr', ['lit', 'at']]]
+           ]
+
+  tree5 = ['RE',
+           ['expr',
+            ['star',
+             ['unit',
+              ['alt',
+               '(',
+               ['unit', ['lit', 'a']],
+               '|',
+               ['unit', ['lit', 'b']],
+               ')']],
+             '*']],
+           ['RE', ['expr', ['opt', ['unit', ['lit', 'c']], '?']]]
+           ]
+  res = convert(tree5)
+  print 'Converted to', res
 
 
-def test():
-  #assert parse_re('hello') == "lit('hello')"
-
-  # parse_re('a*b*c*')
-
+def test_parser():
+  assert parse_re('hello') == "lit('hello')"
   assert parse_re('a*b*c*') == "seq(star(lit('a')), seq(star(lit('b')), star(lit('c'))))"
+  assert parse_re('[ab]*') == "star(oneof('ab'))"
+  assert parse_re('a+(b+c+)') == "seq(plus(lit('a')), seq(plus(lit('b')), plus(lit('c'))))"
+  assert parse_re('[bcfhrsm]at') == "seq(oneof('bcfhrsm'), lit('at'))"
 
-  # assert parse_re('[ab]*') == "star(oneof('ab'))"
-  # assert parse_re('a+(b+c+)') == "seq(plus(lit('a')), seq(plus(lit('b')), plus(lit('c'))))"
-  # assert parse_re('[bcfhrsm]at') == "seq(oneof('bcfhrsm'), lit('at'))"
-  # assert parse_re('(a|b)*c?') == "seq(star(alt(lit('a'), lit('b'))), opt(lit('c')))"
-  # assert parse_re('(a*)?') == "opt(star(lit('a')))"
-  # assert parse_re('abc.$') == "seq(lit('abc'), seq(oneof('?'), eol('$')))"
-  return "tests pass"
+  assert parse_re('(a|b)*c?') == "seq(star(alt(lit('a'), lit('b'))), opt(lit('c')))"
+
+  assert parse_re('(a*)?') == "opt(star(lit('a')))"
+  assert parse_re('abc.$') == "seq(lit('abc'), seq(oneof('?'), eol('$')))"
+  return "Parser tests pass"
 
 
-#print test()
+print test_parser()
 
-print test_convert()
+
+# test_convert()
+
+def match1(pattern, text):
+  """Match pattern against start of text; return longest match found or None.
+  :param pattern: Patterns in APIs format. Ex: star(lit('a'))
+  :param text: The sequence to search from.
+  :return: longest match found or None
+  """
+  remainders = pattern(text)
+  if remainders:
+    shortest = min(remainders, key=len)
+    return text[:len(text) - len(shortest)]
+
+def match(pattern, text):
+  """Match pattern against start of text; return longest match found or None.
+    :param pattern: Patterns in String format. Ex: 'a*'
+    :param text: The sequence to search from.
+    :return: longest match found or None
+    """
+
+  # Parse the string pattern to AST, then to APIs format but in string format.
+  pattern = parse_re(pattern)
+  # Use eval() to convert APIs in string format to lambda functions.
+  pattern = eval(pattern)
+  # Call our general match1() to pattern matching.
+  return match1(pattern, text)
+
+def lit(s): return lambda t: set([t[len(s):]]) if t.startswith(s) else null
+def seq(x, y): return lambda t: set().union(*map(y, x(t)))
+def alt(x, y): return lambda t: x(t) | y(t)
+def oneof(chars): return lambda t: set([t[1:]]) if (t and t[0] in chars) else null
+dot = lambda t: set([t[1:]]) if t else null
+eol = lambda t: set(['']) if t == '' else null
+def star(x): return lambda t: (set([t]) |
+                               set(t2 for t1 in x(t) if t1 != t
+                                   for t2 in star(x)(t1)))
+
+class APIs:
+  def lit(self, s): return lambda t: set([t[len(s):]]) if t.startswith(s) else null
+
+  def seq(x, y): return lambda t: set().union(*map(y, x(t)))
+
+  def alt(x, y): return lambda t: x(t) | y(t)
+
+  def oneof(chars): return lambda t: set([t[1:]]) if (t and t[0] in chars) else null
+
+  dot = lambda t: set([t[1:]]) if t else null
+  eol = lambda t: set(['']) if t == '' else null
+
+  def star(x): return lambda t: (set([t]) |
+                                 set(t2 for t1 in x(t) if t1 != t
+                                     for t2 in star(x)(t1)))
+
+null = frozenset([])
+
+def test_match():
+  assert match1(star(lit('a')), 'aaaaabbbaa') == 'aaaaa'
+  assert match1(lit('hello'), 'hello how are you?') == 'hello'
+  assert match1(lit('x'), 'hello how are you?') is None
+  assert match1(oneof('xyz'), 'x**2 + y**2 = r**2') == 'x'
+  assert match1(oneof('xyz'), '   x is here!') is None
+  return 'Matcher tests pass'
+
+
+print test_match()
+
+def test_engine():
+  assert match('a*', 'aaaaabbbaa') == 'aaaaa'
+  assert match('hello', 'hello how are you?') == 'hello'
+  assert match('[xyz]', 'x**2 + y**2 = r**2') == 'x'
+  assert match('[xyz]', '   x is here!') is None
+
+  return 'Engine tests pass'
+
+print test_engine()
