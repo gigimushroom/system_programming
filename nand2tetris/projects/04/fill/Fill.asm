c403eb508299b2d48c8c6a064690752001f41413
Mushroom King <xiayingp@gmail.com>
2020-07-12 22:50:29 -0700

    week 4 is done

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -6,9 +6,69 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(BLANK)
+(CLEANUP)
+  @temp
+  D=M // D = i
+  @8192
+  D=D-A // D = i - 32
+  @FINISHCLEANUP
+  D;JGE // if i - R1 >= 0, go to FINISH CLEANUP
+
+  @temp
+  D=M // D = Mem[i]
+  @SCREEN
+  D=A+D  // D = 16384 + i
+
+  A=D // Set address to 16384 + i
+  M=0 // Drawing to BLANK
+
+  @temp
+  M=M+1  // i++
+  @CLEANUP
+  0;JMP // goto CLEANUP
+(FINISHCLEANUP)
+
+@temp
+M=0 // reset
+
+(LOOP)
+  @KBD
+  D=M
+  @BLANK
+  D;JEQ // If keyboard is 0, means not pressed, loop again.
+
+  @i
+  M=0 // i = 0
+
+(DRAWLOOP)
+  @i
+  D=M // D = i
+  @8192
+  D=D-A // D = i - 32
+  @DRAWEND
+  D;JGE // if i - R1 >= 0, go to DRAWEND
+
+  @i
+  D=M // D = Mem[i]
+  @SCREEN
+  D=A+D  // D = 16384 + i
+
+  A=D // Set address to 16384 + i
+  M=-1 // Drawing...
+
+  @i
+  M=M+1  // i++
+  @DRAWLOOP
+  0;JMP // goto DRAWLOOP
+(DRAWEND)
+
+  @LOOP
+  0;JMP // go to LOOP
--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -7,3 +7,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+  @i
+  M=0 // i = 0
+  @R2
+  M=0 // sum = 0
+
+(LOOP)
+  @i
+  D=M // D = i
+  @R1
+  D=D-M // D = i - Mem[R1]
+  @END
+  D;JGE // if i - R1 >= 0, go to END
+
+  @R0
+  D=M // temp = Mem[R0]
+  @R2
+  M=M+D // sum = sum + R0
+
+  @i
+  M=M+1  // i++
+  @LOOP
+  0;JMP // Goto LOOP
+
+(END)
+  @END
+  0;JMP // Infinite loop
